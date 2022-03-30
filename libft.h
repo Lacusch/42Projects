f4cd2a3e76f14fa7dadce6582d75f1dd205f64b6
Laszlo M. Szabo <slaszlo-@student.42heilbronn.de>
2022-03-30 20:19:04 +0200

    Expended header

--- libft.h
+++ libft.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   libft.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: slaszlo- <slaszlo-@student.42heilbronn.    +#+  +:+       +#+        */
+/*   By: slaszlo- <coder@slaszlo-@student.42heib    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/29 12:22:43 by slaszlo-          #+#    #+#             */
-/*   Updated: 2022/03/30 11:30:08 by slaszlo-         ###   ########.fr       */
+/*   Updated: 2022/03/30 20:18:53 by slaszlo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,31 @@
 # include <unistd.h>
 
 int		ft_isalpha(int c);
-int		ft_toupper(int c);
 int		ft_isdigit(int c);
-int		ft_tolower(int c);
+int		ft_isalnum(int c);
 int		ft_isascii(int c);
-int ft_strchr(const char *s, int c);
-char	*ft_strjoin(char const	*s1, char const	*s2);
 int		ft_isprint(int c);
+size_t	ft_strlen(const char *s);
+void	*ft_memset(void *b, int c, size_t len);
+void	ft_bezero(void *s, size_t n);
+//memcpy
+//memmove
+//strlcpy
+//strlcat
+
+int		ft_toupper(int c);
+int		ft_tolower(int c);
+int		ft_strchr(const char *s, int c);
+//strrchr
+//strncmp
+//memchr
+//memcmp
+//strnstr
+//atoi
+
+//calloc
+//strdup
+
+char	*ft_strjoin(char const	*s1, char const	*s2);
 
 #endif
