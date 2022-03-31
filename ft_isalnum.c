70e3381fdb182a3037ad6d49829762bd206c44a8
Laszlo M. Szabo <slaszlo-@student.42heilbronn.de>
2022-03-31 10:37:43 +0200

    Improving basic funcitons

--- ft_isalnum.c
+++ ft_isalnum.c
@@ -3,14 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   ft_isalnum.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: slaszlo- <coder@slaszlo-@student.42heib    +#+  +:+       +#+        */
+/*   By: slaszlo- <slaszlo-@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/30 20:21:10 by slaszlo-          #+#    #+#             */
-/*   Updated: 2022/03/30 21:46:52 by slaszlo-         ###   ########.fr       */
+/*   Updated: 2022/03/31 10:24:27 by slaszlo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_isalnum(int c)
-{
+// int	ft_isalnum(int c)
+// {
 
-}
+// }
