eb92863f2c2685a8d2c3792d2e480443b0b1ec24
Laszlo M. Szabo <slaszlo-@student.42heilbronn.de>
2022-04-01 15:31:11 +0200

    strrchr prototype

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,17 +6,25 @@
 /*   By: slaszlo- <slaszlo-@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/01 11:17:34 by slaszlo-          #+#    #+#             */
-/*   Updated: 2022/04/01 13:58:42 by slaszlo-         ###   ########.fr       */
+/*   Updated: 2022/04/01 15:29:46 by slaszlo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-// char	*ft_strrchr(const char *s, int c)
-// {
-// 	int i;
-// 	char *p;
-// 	i = 0;
-// 	while (s[i] != '\0')
-// 	{
-// 		if (s[i] = c)			
-// 	}
-// }
+#include "libft.h"
+
+char	*ft_strrchr(const char *s, int c)
+{
+	size_t	i;
+
+	i = ft_strlen (s);
+	while (i >= 0)
+	{
+		if (s[i] == (char) c)
+		{
+			return ((char *)&s[i]);
+		}
+	i--;
+	}
+	return (NULL);
+}
+
