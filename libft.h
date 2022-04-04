3f3390f668bdc8d02015eb97b91ad318f055af34
Laszlo M. Szabo <slaszlo-@student.42heilbronn.de>
2022-04-04 16:25:11 +0200

    Memmove finished

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: slaszlo- <slaszlo-@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/29 12:22:43 by slaszlo-          #+#    #+#             */
-/*   Updated: 2022/04/04 14:54:45 by slaszlo-         ###   ########.fr       */
+/*   Updated: 2022/04/04 15:24:51 by slaszlo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,19 +24,21 @@ size_t	ft_strlen(const char *s);
 void	*ft_memset(void *b, int c, size_t len);
 void	ft_bzero(void *s, size_t n);
 void	*ft_memcpy(void *dst, const void *src, size_t n);
+void	*ft_memmove(void *dst, const void *src, size_t len);
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
 int		ft_toupper(int c);
 int		ft_tolower(int c);
-char	*ft_stftrchr(const char *s, int c);
+//strchr
 char	*ft_strrchr(const char *s, int c);
+//strncmp
 void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
 int		ft_atoi(const char *str);
 //Only prototype
+
 void	*ft_calloc(size_t count, size_t size);
-//Only prototype
 char	*ft_strdup(const char *s1);
 
 char	*ft_strjoin(char const	*s1, char const	*s2);
