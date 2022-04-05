96d529dff3989c1ae44698c1ea0e0da8a591a09f
Laszlo M. Szabo <slaszlo-@student.42heilbronn.de>
2022-04-05 09:04:54 +0200

    Copying old atoi (from piscine)

---
+++ ft_atoi.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: slaszlo- <coder@slaszlo-@student.42heib    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/05 09:04:25 by slaszlo-          #+#    #+#             */
+/*   Updated: 2022/04/05 09:04:29 by slaszlo-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_atoi(char *str)
+{
+	int	c;
+	int	operator;
+	int	out;
+
+	c = 0;
+	operator = 1;
+	out = 0;
+	while ((str[c] >= '\t' && str[c] <= '\r') || str[c] == ' ')
+		c++;
+	while (str[c] == '+' || str[c] == '-')
+	{
+		if (str[c] == '-')
+			operator = -operator;
+		c++;
+	}
+	while (str[c] >= '0' && str[c] <= '9')
+	{
+		out = (str[c] - '0') + (out * 10);
+		c++;
+	}
+	return (out * operator);
+}
