e0c234ccd0e4b5ddf143c5f3560313e189986292
Laszlo M. Szabo <slaszlo-@student.42heilbronn.de>
2022-04-05 15:13:54 +0200

    strjoin

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,23 +6,34 @@
 /*   By: slaszlo- <slaszlo-@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/29 11:16:06 by slaszlo-          #+#    #+#             */
-/*   Updated: 2022/03/30 10:47:21 by slaszlo-         ###   ########.fr       */
+/*   Updated: 2022/04/05 15:13:13 by slaszlo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-// #include "libft.h"
+#include "libft.h"
 
-// char	*ft_strjoin(char	const	*s1,	char	const	*s2)
-// {
-// 	int c1;
-// 	c1 = 0;
-// 	while (s1[c] != '\0')
-// 	{
-// 		c++;
-// 	}
-// 	char *end;
-// 	*end = *s2 + c;
-// 	while (s2 != '\0')
-// 		*end++ = s2++;
-// 	return ()
-// }
+char	*ft_strjoin(char const	*s1, char const	*s2)
+
+{
+	char	*p;
+	size_t	i;
+	size_t	i2;
+
+	i = 0;
+	i2 = 0;
+	p = ft_calloc(ft_strlen(s1) + ft_strlen(s2) + 1, sizeof(char));
+	if (p == NULL)
+		return (0);
+	while (i < ft_strlen(s1) + ft_strlen(s2))
+	{
+		while (i < ft_strlen(s1))
+		{
+			p[i] = s1[i];
+			i++;
+		}
+		p[i] = s2[i2];
+		i++;
+		i2++;
+	}
+	return (p);
+}
