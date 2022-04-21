da1c180930cfae9a15f40c54d4d77985e665c114
Laszlo M. Szabo <slaszlo-@student.42heilbronn.de>
2022-04-21 18:42:09 +0200

    FINILLY FIXED

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: slaszlo- <slaszlo-@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/04 10:56:24 by slaszlo-          #+#    #+#             */
-/*   Updated: 2022/04/04 14:21:09 by slaszlo-         ###   ########.fr       */
+/*   Updated: 2022/04/21 18:40:47 by slaszlo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,10 +21,10 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 	i = 0;
 	if (ft_strlen (needle) == 0)
 		return ((char *)haystack);
-	while (i < len)
+	while (i < len && *haystack != '\0')
 	{
 		if (0 == ft_memcmp(haystack, needle, needle_len)
-			&& len > i + needle_len)
+			&& len >= i + needle_len)
 		{
 			return ((char *)haystack);
 		}
