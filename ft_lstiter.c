0f4506e96fb6f7d37cdd797966c5dfcf4fcb8596
Laszlo M. Szabo <slaszlo-@student.42heilbronn.de>
2022-04-21 15:40:14 +0200

    fixing norm error

--- ft_lstiter.c
+++ ft_lstiter.c
@@ -6,11 +6,12 @@
 /*   By: slaszlo- <slaszlo-@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/20 14:45:39 by slaszlo-          #+#    #+#             */
-/*   Updated: 2022/04/20 14:58:16 by slaszlo-         ###   ########.fr       */
+/*   Updated: 2022/04/21 15:39:33 by slaszlo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
 void	ft_lstiter(t_list *lst, void (*f)(void *))
 {
 	while (lst != NULL)
